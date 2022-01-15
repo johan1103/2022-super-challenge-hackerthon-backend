080ad76b1a1de1a30af838e60960294624431051
CHO Min HO <chominho96@naver.com>
2022-01-15 21:05:52 +0900

    씹5
    씹

--- INHA_super_challenge_Hackathon_COVID19_project/src/main/java/INHA_super_challenge_hackathon_HitechsFuture/web/member/MemberController.java
+++ INHA_super_challenge_Hackathon_COVID19_project/src/main/java/INHA_super_challenge_hackathon_HitechsFuture/web/member/MemberController.java
@@ -3,8 +3,16 @@ package INHA_super_challenge_hackathon_HitechsFuture.web.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.rmi.ServerException;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,9 +25,18 @@ public class MemberController {
 
     // /members/add 를 받았을 때 실행
     @GetMapping("/add")
-    public String createForm() {
+    public String createForm(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+
         return "members/createForm";
     }
+
+    // 주석
+    /*public String createForm() {
+        return "members/createForm";
+    }*/
 
 
 }
