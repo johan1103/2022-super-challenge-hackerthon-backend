e89d24f1a49516331d82e2048b84b563c775aa36
CHO Min HO <chominho96@naver.com>
2022-01-15 20:48:00 +0900

    씹4
    씹

--- INHA_super_challenge_Hackathon_COVID19_project/src/test/java/INHA_super_challenge_hackathon_HitechsFuture/COVID_19_proj/ChoApplicationTests.java
+++ INHA_super_challenge_Hackathon_COVID19_project/src/test/java/INHA_super_challenge_hackathon_HitechsFuture/COVID_19_proj/ChoApplicationTests.java
@@ -1,4 +1,4 @@
-package INHA_super_challenge_hackathon_HitechsFuture.demo;
+package INHA_super_challenge_hackathon_HitechsFuture.COVID_19_proj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
