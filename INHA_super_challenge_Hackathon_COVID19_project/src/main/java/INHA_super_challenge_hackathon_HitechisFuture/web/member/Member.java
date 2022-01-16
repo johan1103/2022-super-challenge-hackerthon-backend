8f710f8d6e451fa2ff91be8a991cf3507bd44caf
CHO Min HO <chominho96@naver.com>
2022-01-16 23:03:02 +0900

    씹10
    씹

--- INHA_super_challenge_Hackathon_COVID19_project/src/main/java/INHA_super_challenge_hackathon_HitechisFuture/web/member/Member.java
+++ INHA_super_challenge_Hackathon_COVID19_project/src/main/java/INHA_super_challenge_hackathon_HitechisFuture/web/member/Member.java
@@ -2,11 +2,16 @@ package INHA_super_challenge_hackathon_HitechisFuture.web.member;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Entity
 public class Member {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
